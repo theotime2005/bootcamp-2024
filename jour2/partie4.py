7c944ee8d71610ef08c17aa8aeb3b56c52cf4601
theotime2005 <theotime.berthod@epitech.digital>
2024-09-04 14:28:12 +0200

    Jour 2: Partie 4

---
+++ jour2/partie4.py
@@ -0,0 +1,22 @@
+"""
+BootCamp 2024
+Jour 2
+Partie 4
+"""
+
+import pandas as pd
+
+def pandas_csv_read(file: str) -> pd.DataFrame:
+    """
+    Read a csv file with pandas
+    """
+    df = pd.read_csv(file)
+    return df
+
+# --------------------
+def pandas_csv_write(file: str, headers: list, data: list[tuple]):
+    """
+    Write a csv file with pandas
+    """
+    df = pd.DataFrame(data, columns=headers)
+    df.to_csv(file, index=False)
