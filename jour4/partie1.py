d14221bac2bc011c6a07f5fe1ad0d0a7052e99c0
theotime2005 <theotime.berthod@epitech.digital>
2024-09-06 12:01:14 +0200

    Jour4: Partie 1

---
+++ jour4/partie1.py
@@ -0,0 +1,29 @@
+"""
+BootCamp 2024
+Jour 4
+Partie 1
+"""
+
+from pymongo import MongoClient
+
+
+def get_mongo_client(host: str, port: int) -> MongoClient:
+    return MongoClient(host, port)
+
+# ------------------------------
+def get_all_laureates(client: MongoClient) -> list[dict]:
+    return list(client.nobel.laureates.find())
+
+# ------------------------------
+def get_laureates_information(client: MongoClient) -> list[dict]:
+    return list(client.nobel.laureates.find({"prizes.category": "physics"}, {"firstname": 1, "surname": 1, "prizes.category": 1, "_id": 0}))
+
+def get_country_laureates(client: MongoClient, country: str) -> list[dict]:
+    return list(client.nobel.laureates.find({"bornCountry": country}, {"firstname": 1, "surname": 1, "bornCountry": 1, "_id": 0}))
+
+# ------------------------------
+get_shared_prizes(client: MongoClient) -> list[dict]:
+    return list(client.nobel.laureates.find({"prizes.share": {"$gt": 1}}, {"firstname": 1, "surname": 1, "prizes.share": 1, "_id": 0}))
+
+def get_shared_prizes_common(client: MongoClient) -> list[dict]:
+    return list(client.nobel.laureates.find({"prizes.share": 1}, {"firstname": 1, "surname": 1, "prizes.share": 1, "_id": 0}))
