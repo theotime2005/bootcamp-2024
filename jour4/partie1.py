a758061c6ef965249ce82054903b803a9b0a2187
theotime2005 <theotime.berthod@epitech.digital>
2024-09-12 16:52:53 +0200

    wip

--- jour4/partie1.py
+++ jour4/partie1.py
@@ -15,7 +15,19 @@ def get_all_laureates(client: MongoClient) -> list[dict]:
 
 # ------------------------------
 def get_laureates_information(client: MongoClient) -> list[dict]:
-    return list(client.nobel.laureates.find({"prizes.category": "physics"}, {"firstname": 1, "surname": 1, "prizes.category": 1, "_id": 0}))
+    return list(client.nobel.laureates.find({}, {'firstname': 1, 'surname': 1, 'born': 1, '_id': 0}))
+
+def get_prize_categories(client: MongoClient) -> list[str]:
+    data = list(client.nobel.prizes.find({}, {'category': 1, '_id': 0}))
+    result = []
+    for category in data:
+        if not category['category'] in result:
+            result.append(category['category'])
+    return result
+
+# ------------------------------
+def get_category_laureates(client: MongoClient, category: str) -> list[dict]:
+    
 
 def get_country_laureates(client: MongoClient, country: str) -> list[dict]:
     return list(client.nobel.laureates.find({"bornCountry": country}, {"firstname": 1, "surname": 1, "bornCountry": 1, "_id": 0}))
